50ed64a3aca3234ead58191a34638143001a4519
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-08-09 16:50:59 -0300

    criacao de algumas paginas, componentes , estilizacao global e tema

--- src/assets/pages/Details/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Container } from "./styles.js"
-export function Details() {
-  return (
-    <Container>Hello World!</Container>
-  )
-}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles.js"
+import { Button } from '../../components/Button'
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+      <Button title="entrar" />
+      <Button title="cadastrar" />
+      <Button title="voltar" />
+
+    </Container>
+  )
+}
