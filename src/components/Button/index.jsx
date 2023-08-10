6b8a24e213a3c0b3c8d81285a81d0c5acc51b9aa
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-08-10 11:48:31 -0300

    adicionado componente de texto clicavel. É um botao mas que é so um texto

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,9 @@
 import { Container } from './styles'
 
 export function Button({ title, loading = false, ...rest }) {
-    // o rest operator serve para enviar as propriedades de um botao sem ter q especificar.
-    //Por exemplo, o botao tem inumeras propriedades. Caso queira enviar todas elas para o componente, mas nao especificar tudo, essas propriedades estarao
+    // o rest operator serve para enviar as propriedades de um componente sem ter q especificar.
+    //Por exemplo, o botao tem inumeras propriedades. Caso queira enviar alguma outra propriedade para o componente quando for utilizar, 
+    //mas nao ter que especificar tudo aqui no jsx, essas propriedades estarao
     //todas dentro do rest. Dai as que quiser deixar explicitas pra fazer algo, é so colocar normal, assim como está agora.
     return (
         <Container
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,12 +3,15 @@ import { Button } from '../../components/Button'
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag/index.jsx"
+import { ClickableText } from "../../components/ClickableText/index.jsx"
 
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <ClickableText title="Excluir nota" />
 
       <Section title="Links úteis">
         <Links>
@@ -20,7 +23,6 @@ export function Details() {
       <Section title="Marcadores">
         <Tag title="express" />
         <Tag title="nodejs" />
-
       </Section>
 
       <Button title="voltar" />
