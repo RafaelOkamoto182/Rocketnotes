ffc879ee36c8a92556a524fa71e10e5880b7798d
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-08-10 11:22:15 -0300

    criado componente "tag"

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from "./styles.js"
 import { Button } from '../../components/Button'
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag/index.jsx"
 
 
 export function Details() {
@@ -10,11 +11,15 @@ export function Details() {
       <Header />
 
       <Section title="Links úteis">
-
         <Links>
           <li><a href="#">https://www.rocketseat.com.br</a></li>
           <li><a href="#">https://www.rocketseat.com.br</a></li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
 
       </Section>
 
