cc2a3e91bcce6843beacd5b2a6d3db0bd4d9ae90
RafaelOkamoto182 <rafa.mg1@hotmail.com>
2023-08-11 15:56:05 -0300

    finalizada pagina de login e pagina de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,19 +1,25 @@
-import { FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 
 import { Background, Container, Form } from './styles'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-export function SignIn() {
+export function SignUp() {
     return (
         <Container>
+            <Background />
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>A place to store and manage your useful links.</p>
 
-                <h2>Sign In</h2>
+                <h2>Create your account</h2>
 
+                <Input
+                    placeholder="Name"
+                    type="text"
+                    icon={FiUser}
+                />
                 <Input
                     placeholder="E-mail"
                     type="text"
@@ -25,12 +31,12 @@ export function SignIn() {
                     icon={FiLock}
                 />
 
-                <Button title="Sign In" />
+                <Button title="Sign Up" />
 
-                <a href="#">New Account</a>
+                <a href="#">Already have an account? Sign In</a>
             </Form>
 
-            <Background />
+
 
         </Container>
     )
